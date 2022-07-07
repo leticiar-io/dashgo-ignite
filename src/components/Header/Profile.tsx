6ab557858a19fecb352d4502dca91ebcc93c6567
leticiar-io <leticia.rodrigues20019@gmail.com>
2022-07-07 17:04:51 -0300

    feat: Adicionando o componente Header

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,24 @@
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
+  return (
+    <Flex align={"center"}>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Letícia Rodrigues</Text>
+          <Text color="gray.300" fontSize="small">
+            leticia.rodrigues20021@gmail.com
+          </Text>
+        </Box>
+      )}
+      <Avatar
+        size="md"
+        name="Letícia Rodrigues"
+        src="https://github.com/leticiar-io.png"
+      />
+    </Flex>
+  );
+}
