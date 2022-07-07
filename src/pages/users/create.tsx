2beab3649e6bbea4708f27caca343fd7019cdc57
leticiar-io <leticia.rodrigues20019@gmail.com>
2022-07-07 17:07:52 -0300

    feat: criando o formulário de criacao de usuarios

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,138 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  SimpleGrid,
+  VStack
+} from "@chakra-ui/react";
+import Link from "next/link";
+import { Input } from "../../components/form/Input";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+import { SubmitHandler, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { useRouter } from "next/router";
+
+type CreateUserFormData = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+};
+
+const CreateUserFormSchema = yup.object().shape({
+  name: yup.string().required("Nome obrigatório"),
+  email: yup.string().required("E-mail obrigatório").email("E-mail inválido"),
+  password: yup
+    .string()
+    .required("Senha obrigatória")
+    .min(6, "Minímo 6 caracteres"),
+  password_confirmation: yup
+    .string()
+    .oneOf([null, yup.ref("password")], "Senhas não coincidem")
+});
+
+export default function CreateUser() {
+  const router = useRouter();
+
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(CreateUserFormSchema)
+  });
+
+  const { errors } = formState;
+
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000)); // depois de 2 segundos dá um console.log
+    console.log(values);
+  };
+
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6" onSubmit={handleSubmit(handleCreateUser)}>
+        <Sidebar />
+
+        <Box
+          as="form"
+          flex="1"
+          borderRadius={8}
+          bg="gray.800"
+          p={["4", "6", "8"]}
+        >
+          <Heading size="lg" fontWeight="normal">
+            Criar Usuário
+          </Heading>
+          <Divider my="6" borderColor="gray.700" />
+          <VStack spacing={["4", "6", "8"]}>
+            <SimpleGrid
+              minChildWidth="240px"
+              spacing={["4", "6", "8"]}
+              w="100%"
+            >
+              <Input
+                name="name"
+                label="Nome Completo"
+                error={errors.name}
+                {...register("name")}
+              />
+              <Input
+                name="email"
+                label="E-mail"
+                type="email"
+                error={errors.email}
+                {...register("email")}
+              />
+            </SimpleGrid>
+            <SimpleGrid
+              minChildWidth="240px"
+              spacing={["4", "6", "8"]}
+              w="100%"
+            >
+              <Input
+                name="password"
+                label="Senha"
+                type="password"
+                error={errors.password}
+                {...register("password")}
+              />
+              <Input
+                name="password_confirmation"
+                label="Confirme sua senha"
+                type="password"
+                error={errors.password_confirmation}
+                {...register("password_confirmation")}
+              />
+            </SimpleGrid>
+          </VStack>
+
+          <Flex mt="8" justify="flex-end">
+            <HStack spacing="4">
+              <Link href="/users" passHref>
+                <Button
+                  as="a"
+                  colorScheme="whiteAlpha"
+                  disabled={formState.isSubmitting}
+                >
+                  Cancelar
+                </Button>
+              </Link>
+              <Button
+                type="submit"
+                colorScheme="purple"
+                isLoading={formState.isSubmitting}
+              >
+                Salvar
+              </Button>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
