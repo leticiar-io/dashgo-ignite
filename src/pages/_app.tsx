7712d6ce296167d1007141cb8a5e7b76c520dae5
leticiar-io <leticia.rodrigues20019@gmail.com>
2022-07-07 17:08:13 -0300

    feat: adicionando o react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,31 @@
-import { AppProps } from "next/app"
+import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  )
+import { SidebarDrawerProvider } from "../components/contexts/SidebarDrawerContext";
+
+import { makeServer } from "../services/mirage/index";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+
+if (process.env.NODE_ENV === "development") {
+  makeServer();
 }
 
-export default MyApp
+const queryClient = new QueryClient();
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
