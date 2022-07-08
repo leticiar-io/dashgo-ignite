09d1c309828239182c0c0c35b5ab3cdf0565e198
leticiar-io <leticia.rodrigues20019@gmail.com>
2022-07-08 08:54:25 -0300

    feat: utilizando mutations

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -9,6 +9,8 @@ import {
   VStack
 } from "@chakra-ui/react";
 import Link from "next/link";
+import { useMutation } from "react-query";
+
 import { Input } from "../../components/form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
@@ -18,6 +20,8 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 
 import { useRouter } from "next/router";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
 
 type CreateUserFormData = {
   name: string;
@@ -46,11 +50,28 @@ export default function CreateUser() {
   });
 
   const { errors } = formState;
+  
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post("users", {
+      user: {
+        ... user,
+        created_at: new Date(),
+      }
+    })
+
+    return response.data.user;
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("users")
+    }
+  });
 
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000)); // depois de 2 segundos dá um console.log
-    console.log(values);
+    await createUser.mutateAsync(values);
+
+    router.push("/users")
   };
+
 
   return (
     <Box>
